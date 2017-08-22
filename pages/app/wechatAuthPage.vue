3f5e22526caf870ee07e82b50f1c277ce366c7a2
leiyu0932 <leiyu0932@163.com>
2017-08-22 15:44:09 +0800

    S

--- pages/app/wechatAuthPage.vue
+++ pages/app/wechatAuthPage.vue
@@ -1,22 +1,24 @@
 <template>
   <div>
     wechatAuthPage
+    <br> {{ info }}
   </div>
 </template>
 <script>
 // import axios from '~/utils/axiosCross'
 
 export default {
-  name: 'wechatAuthPage'
-  // async asyncData (redirect, query) {
-  //   const { data } = await axios.get('xxx')
-  //   if (data.status === 200) {
-  //     redirect(query.red)
-  //   }
-  //   return {
-  //     info: data
-  //   }
-  // }
+  name: 'wechatAuthPage',
+  async asyncData ({ query }) {
+    // const { data } = await axios.get('xxx')
+    // if (data.status === 200) {
+    //   redirect(query.red)
+    // }
+    console.log(query)
+    return {
+      info: query
+    }
+  }
 }
 </script>
 <style scoped>
