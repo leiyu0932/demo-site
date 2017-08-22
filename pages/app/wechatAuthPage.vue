973756e7d39a93f5e0a8ffc35ea81bd94e38665a
leiyu0932 <leiyu0932@163.com>
2017-08-22 15:26:59 +0800

    s

--- pages/app/wechatAuthPage.vue
+++ pages/app/wechatAuthPage.vue
@@ -4,19 +4,19 @@
   </div>
 </template>
 <script>
-import axios from '~/utils/axiosCross'
+// import axios from '~/utils/axiosCross'
 
 export default {
-  name: 'wechatAuthPage',
-  async asyncData (redirect, query) {
-    const { data } = await axios.get('app/home')
-    if (data.status === 200) {
-      redirect(query.red)
-    }
-    return {
-      info: data
-    }
-  }
+  name: 'wechatAuthPage'
+  // async asyncData (redirect, query) {
+  //   const { data } = await axios.get('xxx')
+  //   if (data.status === 200) {
+  //     redirect(query.red)
+  //   }
+  //   return {
+  //     info: data
+  //   }
+  // }
 }
 </script>
 <style scoped>
