f570210871e941e80c65e9e51e0a92512016d8c9
leiyu0932 <leiyu0932@163.com>
2017-08-23 10:11:44 +0800

    s

--- middleware/apitest.js
+++ middleware/apitest.js
@@ -5,13 +5,14 @@ export default async function (context) {
   const config = {}
   if (context.isServer && context.req && context.req.headers.cookie) {
     const cookie = context.req.headers.cookie
+    console.log(cookie)
     config.headers = {
       'X-Requested-With': 'XMLHttpRequest',
       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
       cookie
     }
   }
-  const response = await axios.get('ajax/weixin/demo/auth?p1=a&p2=b', config)
+  const response = await axios.get('ajax/weixin/auth/info?p1=a&p2=b', config)
   console.log(response.data.status)
   // 默认回调函数
   const callback = data => {
