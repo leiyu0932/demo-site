4a74d06b658969dfa5b634197c6bec7f13c1f811
leiyu0932 <leiyu0932@163.com>
2017-08-23 11:52:59 +0800

    s

--- pages/app/user/index.vue
+++ pages/app/user/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="user" class="body">
+  <div v-if="sss" id="user" class="body">
     <div>
       {{ sss }}
       <br>
@@ -10,6 +10,7 @@
 </template>
 <script>
 import axios from '~/utils/axiosCross'
+import { handleApi } from '~/utils/handle'
 import Bottom from '~/components/Bottom'
 
 export default {
@@ -18,24 +19,30 @@ export default {
     Bottom
   },
   // middleware: 'auth'
-  async asyncData ({ isServer, req }) {
+  async asyncData (context) {
     // xsrfCookieName: 'XSRF-TOKEN'
     const config = {}
-    if (isServer && req && req.headers.cookie) {
-      const cookie = req.headers.cookie
+    if (context.isServer && context.req && context.req.headers.cookie) {
+      const cookie = context.req.headers.cookie
       config.headers = {
         'X-Requested-With': 'XMLHttpRequest',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         cookie
       }
     }
-    let { data } = await axios.get('ajax/user/info', config)
-    return {
-      sss: data
+    let response = await axios.get('ajax/user/info', config)
+    let sss = null
+    const callback = data => {
+      sss = data
     }
-  },
-  beforeCreate () {
-    console.log(process.env.NODE_ENV)
+    handleApi({
+      context,
+      response,
+      callback
+    })
+    return {
+      sss
+    }
   },
   methods: {
     async logout () {
@@ -43,6 +50,7 @@ export default {
       console.log(data)
       if (data.status === 200) {
         console.log(data.data.session.name)
+        this.$store.commit('SET_USER', null)
         this.$router.push('/app/user/login')
       }
     }
